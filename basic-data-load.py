a5c1ce8b341bd6523622b09c008475de5b8369e8
werkel61 <github@bramderuyck.be>
2020-01-14 16:26:35 +0100

    topic naar cloudMQTT versturen.

--- basic-data-load.py
+++ basic-data-load.py
@@ -5,55 +5,65 @@ Connect to InfluxDB 2.0 - write data and query them
 from datetime import datetime
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
+from random import uniform
+from time import sleep
 
 my_token = "D6ltGUKEgdSCOU9FgswD6Zp6H8faQ7MsdEE3JevpmTN-32JJSdATdyNfCOb_6SguWeAzd29NkFd6iid-mYfX9g=="
 ## You can generate a Token from the "Tokens Tab" in the UI
-client = InfluxDBClient(url="https://eu-central-1-1.aws.cloud2.influxdata.com", token=my_token)
 
 
-bucket = "influxdata's Bucket"
-# This is the standard bucket name
-org = 'influxdata@bramderuyck.be'
-# Your organization name is your e-mail address in the free version
+def send_basic():
+	client = InfluxDBClient(url="https://eu-central-1-1.aws.cloud2.influxdata.com", token=my_token)
 
-try:
-	kind = 'temperature'
-	host = 'host1'
-	device = 'opt-123'
-	"""
-	Write data by Point structure
-	"""
-	point = Point(kind).tag('host', host).tag('device', device).field('value', 25.3).time(time=datetime.utcnow())
+	bucket = "influxdata's Bucket"
+	# This is the standard bucket name
+	org = 'influxdata@bramderuyck.be'
+	# Your organization name is your e-mail address in the free version
 
-	print(f'Writing to InfluxDB cloud: {point.to_line_protocol()} ...')
+	try:
+		kind = 'temperature'
+		host = 'host1'
+		device = 'opt-123'
+		"""
+		Write data by Point structure
+		"""
 
-	write_api = client.write_api(write_options=SYNCHRONOUS)
-	write_api.write(bucket=bucket, org=org, record=point)
+		point = Point(kind).tag('host', host).tag('device', device).field('value', uniform(23.0, 27.0)).time(time=datetime.utcnow())
 
-	print()
-	print('success')
-	print()
-	print()
+		print(f'Writing to InfluxDB cloud: {point.to_line_protocol()} ...')
 
-	"""
-	Query written data
-	"""
-	query = f'from(bucket: "{bucket}") |> range(start: -1d) |> filter(fn: (r) => r._measurement == "{kind}")'
-	print(f'Querying from InfluxDB cloud: "{query}" ...')
-	print()
+		write_api = client.write_api(write_options=SYNCHRONOUS)
+		write_api.write(bucket=bucket, org=org, record=point)
 
-	query_api = client.query_api()
-	tables = query_api.query(query=query, org=org)
+		print()
+		print('success')
+		print()
+		print()
 
-	for table in tables:
-		for row in table.records:
-			print(f'{row.values["_time"]}: host={row.values["host"]},device={row.values["device"]} '
-				  f'{row.values["_value"]} °C')
+		"""
+		Query written data
+		"""
+		query = f'from(bucket: "{bucket}") |> range(start: -1d) |> filter(fn: (r) => r._measurement == "{kind}")'
+		print(f'Querying from InfluxDB cloud: "{query}" ...')
+		print()
 
-	print()
-	print('success')
-except Exception as e:
-    print(e)
-finally:
-    client.close()
+		query_api = client.query_api()
+		tables = query_api.query(query=query, org=org)
 
+		for table in tables:
+			for row in table.records:
+				print(f'{row.values["_time"]}: host={row.values["host"]},device={row.values["device"]} '
+					  f'{row.values["_value"]} °C')
+
+		print()
+		print('success')
+	except Exception as e:
+		print(e)
+	finally:
+		client.close()
+	return
+
+
+while True:
+	send_basic()
+	sleep(1)
---
+++ publish_to_topic.py
@@ -0,0 +1,29 @@
+import paho.mqtt.client as mqtt
+import time
+from random import uniform
+
+# from sense_hat import SenseHat
+# sense = SenseHat()
+# set up mqtt client
+client = mqtt.Client("python_pub")
+#set mqtt username/pw
+client.username_pw_set(username="gkpdrpfa", password="bvwnbQm7g5Wv")
+#set server to publish to
+client.connect("hairdresser.cloudmqtt.com", 18875)
+client.loop_start()
+# What is this?
+# http://www.steves-internet-guide.com/loop-python-mqtt-client/
+
+try:
+	while True:
+		#publish temp to topic
+		#client.publish("sense/temp", sense.get_temperature())
+		client.publish("sense/temp", uniform(3.0, 27.0))
+		#publish humidity
+		#client.publish("sense/humid", sense.get_humidity())
+		#pause for 10 seconds
+		time.sleep(1)
+	#deal nicely with ^C
+except KeyboardInterrupt:
+	print("interrupted!")
+client.loop_stop()
